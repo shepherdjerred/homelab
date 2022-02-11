29844dc63f5b351864bc05664ca78c25900bedeb
Jerred Shepherd <shepherdjerred@gmail.com>
2022-02-10 22:10:23 -0800

    Add Zeus bootstrap

---
+++ zeus/bootstrap/create-iso.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ISO_FILE=coreos.iso
+IGNITION_FILE=ignition.ign
+
+butane --pretty --strict butane.bu > "$IGNITION_FILE"
+
+docker run --rm \
+  -v "$(pwd)"/"$ISO_FILE":/"$ISO_FILE" \
+  -v "$(pwd)"/"$IGNITION_FILE":/"$IGNITION_FILE" \
+  quay.io/coreos/coreos-installer:release \
+  iso ignition embed -i "$IGNITION_FILE" "$ISO_FILE"
+
+dd if="$(ISO_FILE)" of=/dev/disk1s1
