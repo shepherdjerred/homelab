b072127595037498c288fc81e5767023f3c29d74
Jerred Shepherd <shepherdjerred@gmail.com>
2022-02-10 22:43:10 -0800

    Force ignition

--- zeus/bootstrap/create-iso.sh
+++ zeus/bootstrap/create-iso.sh
@@ -8,4 +8,4 @@ docker run --rm \
   -v "$(pwd)"/"$ISO_FILE":/"$ISO_FILE" \
   -v "$(pwd)"/"$IGNITION_FILE":/"$IGNITION_FILE" \
   quay.io/coreos/coreos-installer:release \
-  iso ignition embed -i "$IGNITION_FILE" "$ISO_FILE"
+  iso ignition embed -i "$IGNITION_FILE" "$ISO_FILE" -f
