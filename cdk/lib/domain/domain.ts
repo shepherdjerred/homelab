2cbb2273ab43878179af0180bbb1f394ede0bcde
Jerred Shepherd <shepherdjerred@gmail.com>
2022-08-06 13:16:24 -0700

    Add sjer.red

--- cdk/lib/domain/domain.ts
+++ cdk/lib/domain/domain.ts
@@ -2,9 +2,12 @@ import { Duration, Stack } from "aws-cdk-lib";
 import {
   AaaaRecord,
   ARecord,
+  CnameRecord,
   HostedZone,
   IHostedZone,
+  MxRecord,
   RecordTarget,
+  TxtRecord,
 } from "aws-cdk-lib/aws-route53";
 
 interface Association {
@@ -19,14 +22,94 @@ interface Address {
 }
 
 export function createDomainResources(stack: Stack) {
-  const root = "shepherdjerred.com";
   const hostedZone = HostedZone.fromHostedZoneAttributes(stack, "HostedZone", {
     hostedZoneId: "Z24MJMG74F2S94",
-    zoneName: root,
+    zoneName: "shepherdjerred.com",
   });
+  const shortHostedZone = HostedZone.fromHostedZoneAttributes(
+    stack,
+    "ShortHostedZone",
+    {
+      hostedZoneId: "Z052318621SQJ9EM5RPOU",
+      zoneName: "sjer.red",
+    }
+  );
+  const zones = [hostedZone, shortHostedZone];
+  zones.forEach((zone) => {
+    createAllRecords(stack, zone);
+  });
+}
+
+function createAllRecords(stack: Stack, hostedZone: IHostedZone) {
   createZeusRecords(stack, hostedZone);
   createPersephoneRecords(stack, hostedZone);
   createRouterRecords(stack, hostedZone);
+  createKittensRecords(stack, hostedZone);
+  createResumeRecords(stack, hostedZone);
+  createPersonalSiteRecords(stack, hostedZone);
+}
+
+function createKittensRecords(stack: Stack, hostedZone: IHostedZone) {
+  new CnameRecord(stack, `kitten-${hostedZone.zoneName}`, {
+    recordName: "kittens",
+    domainName: "sj-kittens.netlify.app",
+    ttl: Duration.minutes(1),
+    zone: hostedZone,
+  });
+}
+
+function createResumeRecords(stack: Stack, hostedZone: IHostedZone) {
+  new CnameRecord(stack, `resume-${hostedZone.zoneName}`, {
+    recordName: "resume",
+    domainName: "d1sx9sqvxo7ng4.cloudfront.net.",
+    ttl: Duration.minutes(1),
+    zone: hostedZone,
+  });
+}
+
+function createPersonalSiteRecords(stack: Stack, hostedZone: IHostedZone) {
+  new CnameRecord(stack, `personal-www-${hostedZone.zoneName}`, {
+    recordName: "www",
+    domainName: "shepherdjerred.netlify.app",
+    ttl: Duration.minutes(1),
+    zone: hostedZone,
+  });
+  new ARecord(stack, `personal-root-${hostedZone.zoneName}`, {
+    target: RecordTarget.fromIpAddresses("75.2.60.5"),
+    ttl: Duration.minutes(1),
+    zone: hostedZone,
+  });
+}
+
+function createEmailRecords(stack: Stack, hostedZone: IHostedZone) {
+  const name = hostedZone.zoneName;
+  new MxRecord(stack, `mx-${name}`, {
+    values: [
+      {
+        priority: 10,
+        hostName: "in1-smtp.messagingengine.com",
+      },
+      {
+        priority: 20,
+        hostName: "in2-smtp.messagingengine.com",
+      },
+    ],
+    ttl: Duration.minutes(1),
+    zone: hostedZone,
+  });
+  new TxtRecord(stack, `spf-${name}`, {
+    values: ["v=spf1 include:spf.messagingengine.com ?all"],
+    ttl: Duration.minutes(1),
+    zone: hostedZone,
+  });
+  [1, 2, 3].forEach((i) => {
+    new CnameRecord(stack, `dkim-${i}-${name}`, {
+      recordName: `fm${i}._domainkey.${name}`,
+      domainName: `fm${i}.${name}.dkim.fmhosted.com`,
+      ttl: Duration.minutes(1),
+      zone: hostedZone,
+    });
+  });
 }
 
 function createRouterRecords(stack: Stack, hostedZone: IHostedZone) {
@@ -155,6 +238,7 @@ function createRecords(
         const resourceName = (
           domain +
           association.base +
+          hostedZone.zoneName +
           address.addressType
         ).replaceAll(".", "");
         switch (address.addressType) {
