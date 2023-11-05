1bfb66b9457785789db01f1b6635b7979ab8a6fc
Jerred Shepherd <github@sjer.red>
2023-11-05 00:37:02 -0700

    minimum with deno

--- main.test.ts
+++ main.test.ts
@@ -1,11 +1,10 @@
 import { MyChart } from "./main.ts";
 import { Testing } from "npm:cdk8s";
+import { assertSnapshot } from "https://deno.land/std@0.205.0/testing/snapshot.ts";
 
-describe("Placeholder", () => {
-  test("Empty", () => {
-    const app = Testing.app();
-    const chart = new MyChart(app, "test-chart");
-    const results = Testing.synth(chart);
-    expect(results).toMatchSnapshot();
-  });
+Deno.test("placeholder", async (t) => {
+  const app = Testing.app();
+  const chart = new MyChart(app, "test-chart", "label");
+  const results = Testing.synth(chart);
+  await assertSnapshot(t, results);
 });
--- main.ts
+++ main.ts
@@ -1,14 +1,33 @@
-import { App, Chart, ChartProps } from "npm:cdk8s";
+import { App, Chart } from "npm:cdk8s";
 import { Construct } from "npm:constructs";
+import { KubeDeployment } from "./imports/k8s.ts";
 
 export class MyChart extends Chart {
-  constructor(scope: Construct, id: string, props: ChartProps = {}) {
-    super(scope, id, props);
+  constructor(scope: Construct, ns: string, appLabel: string) {
+    super(scope, ns);
 
-    // define resources here
+    new KubeDeployment(this, "my-deployment", {
+      spec: {
+        replicas: 3,
+        selector: { matchLabels: { app: appLabel } },
+        template: {
+          metadata: { labels: { app: appLabel } },
+          spec: {
+            containers: [
+              {
+                name: "app-container",
+                image: "nginx:1.19.10",
+                ports: [{ containerPort: 80 }],
+              },
+            ],
+          },
+        },
+      },
+    });
   }
 }
 
 const app = new App();
-new MyChart(app, "inner");
+new MyChart(app, "getting-started", "my-app");
+
 app.synth();
