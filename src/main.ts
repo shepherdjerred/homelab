5ea4912cae8b8bdc79354970f35fdfbf936bd474
Jerred Shepherd <github@sjer.red>
2023-11-06 21:48:11 -0800

    plex

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { App, Chart } from "cdk8s";
 import { createInvidiousDeployment } from "./invidious.ts";
 import { createTedditDeployment } from "./teddit.ts";
+import { createPlexDeployment } from "./plex.ts";
 
 const app = new App();
 const chart = new Chart(app, "jerred");
 
 createInvidiousDeployment(chart);
 createTedditDeployment(chart);
+createPlexDeployment(chart);
 
 app.synth();
---
+++ src/plex.ts
@@ -0,0 +1,67 @@
+import { Deployment, EnvValue, Protocol } from "cdk8s-plus-27";
+import { Chart } from "cdk8s";
+
+export function createPlexDeployment(chart: Chart) {
+  const plexDeployment = new Deployment(chart, "plex", {
+    replicas: 1,
+  });
+
+  plexDeployment.addContainer({
+    image: "plexinc/pms-docker",
+    ports: [
+      {
+        name: "32400",
+        number: 32400,
+        protocol: Protocol.TCP,
+      },
+      {
+        name: "3005",
+        number: 3005,
+        protocol: Protocol.TCP,
+      },
+      {
+        name: "8324",
+        number: 8324,
+        protocol: Protocol.TCP,
+      },
+      {
+        name: "32469",
+        number: 32469,
+        protocol: Protocol.TCP,
+      },
+      {
+        name: "32410",
+        number: 32410,
+        protocol: Protocol.UDP,
+      },
+      {
+        name: "32412",
+        number: 32412,
+        protocol: Protocol.UDP,
+      },
+      {
+        name: "32413",
+        number: 32413,
+        protocol: Protocol.UDP,
+      },
+      {
+        name: "32414",
+        number: 32414,
+        protocol: Protocol.UDP,
+      },
+    ],
+    envVariables: {
+      POSTGRES_PASSWORD: EnvValue.fromValue("password"),
+      POSTGRES_DB: EnvValue.fromValue("invidious"),
+    },
+    securityContext: {
+      ensureNonRoot: false,
+      readOnlyRootFilesystem: false,
+    },
+  });
+
+  const plexService = plexDeployment.exposeViaService();
+
+  plexService.metadata.addAnnotation("tailscale.com/expose", "true");
+  plexService.metadata.addAnnotation("tailscale.com/hostname", "plex");
+}
