c2fa35ee943747f86f8b200f0ca459319e2ef06d
Jerred Shepherd <github@sjer.red>
2023-11-12 06:28:46 +0000

    rearrange

---
+++ cdk8s/src/main.ts
@@ -0,0 +1,33 @@
+import { App, Chart } from "cdk8s";
+import { createInvidiousDeployment } from "./resources/invidious.ts";
+import { createTedditDeployment } from "./resources/teddit.ts";
+import { createPlexDeployment } from "./resources/plex.ts";
+import { createRadarrDeployment } from "./resources/radarr.ts";
+import { createBazarrDeployment } from "./resources/bazarr.ts";
+import { createHomeAssistantDeployment } from "./resources/homeassistant.ts";
+import { createJackettDeployment } from "./resources/jackett.ts";
+import { createNitterDeployment } from "./resources/nitter.ts";
+import { createOverseerrDeployment } from "./resources/overseerr.ts";
+import { createQBitTorrentDeployment } from "./resources/qbittorrent.ts";
+import { createSonarrDeployment } from "./resources/sonarr.ts";
+import { createSyncthingDeployment } from "./resources/syncthing.ts";
+import { createTautulliDeployment } from "./resources/tautulli.ts";
+
+const app = new App();
+const chart = new Chart(app, "turing");
+
+createInvidiousDeployment(chart);
+createTedditDeployment(chart);
+createPlexDeployment(chart);
+createRadarrDeployment(chart);
+createBazarrDeployment(chart);
+createHomeAssistantDeployment(chart);
+createJackettDeployment(chart);
+createNitterDeployment(chart);
+createOverseerrDeployment(chart);
+createQBitTorrentDeployment(chart);
+createSonarrDeployment(chart);
+createSyncthingDeployment(chart);
+createTautulliDeployment(chart);
+
+app.synth();
