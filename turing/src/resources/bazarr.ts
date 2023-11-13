d4637244564fdedf19e077892eac56a3fb4ca5a4
Jerred Shepherd <github@sjer.red>
2023-11-13 01:03:53 +0000

    fix bazaar

--- turing/src/resources/bazarr.ts
+++ turing/src/resources/bazarr.ts
@@ -1,4 +1,9 @@
-import { Deployment, Ingress, IngressBackend, Service } from "npm:cdk8s-plus-27";
+import {
+  Deployment,
+  Ingress,
+  IngressBackend,
+  Service,
+} from "npm:cdk8s-plus-27";
 import { ApiObject, Chart } from "npm:cdk8s";
 
 export function createBazarrDeployment(chart: Chart) {
@@ -27,18 +32,18 @@ export function createBazarrDeployment(chart: Chart) {
     ],
   });
 
-  const ingress = new Ingress(chart, "bazarr-ingress", {
-    defaultBackend: IngressBackend.fromService(service),
-  });
+  // const ingress = new Ingress(chart, "bazarr-ingress", {
+  //   defaultBackend: IngressBackend.fromService(service),
+  // });
 
   // TODO
   // https://cdk8s.io/docs/latest/basics/escape-hatches/#patching-api-objects-directly
   // https://tailscale.com/kb/1236/kubernetes-operator/#ingress-resource
   // ApiObject.of(ingress).addJsonPatch();
 
-  deployment.exposeViaIngress("/", {
-    ingress,
-  });
+  // deployment.exposeViaIngress("/", {
+  //   ingress,
+  // });
 
   service.metadata.addAnnotation("tailscale.com/expose", "true");
   service.metadata.addAnnotation("tailscale.com/hostname", "bazarr");
