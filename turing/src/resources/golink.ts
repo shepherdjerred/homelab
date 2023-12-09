3f90d65cefc2f4f52f10ce1f0f9646c0f9b126ae
Jerred Shepherd <github@sjer.red>
2023-12-09 22:07:30 +0000

    update key

--- turing/src/resources/golink.ts
+++ turing/src/resources/golink.ts
@@ -29,7 +29,7 @@ export function createGolinkDeployment(chart: Chart) {
     envVariables: {
       TS_AUTH_KEY: EnvValue.fromSecretValue({
         secret: Secret.fromSecretName(chart, "tailscale-auth-key", "name"),
-        key: ".",
+        key: "credential",
       }),
     },
     resources: {},
