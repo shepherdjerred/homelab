3164bb69a91b97ebd80b2e82836c439a83ac357d
Jerred Shepherd <github@sjer.red>
2023-12-12 22:07:29 +0000

    watch

--- turing/scripts.config.ts
+++ turing/scripts.config.ts
@@ -2,7 +2,7 @@ import type { DenonConfig } from "https://deno.land/x/denon@2.5.0/mod.ts";
 
 export const config: DenonConfig = {
   scripts: {
-    watch: {
+    diff: {
       cmd: "deno task build && deno task diff",
     },
     dev: {
