46d715564f41529edbcbc8daabc5644093527ba4
Jerred Shepherd <github@sjer.red>
2023-12-31 21:08:35 +0000

    setup pikaraoke

--- cdk8s/src/services/torrents/bitmagnet.ts
+++ cdk8s/src/services/torrents/bitmagnet.ts
@@ -31,7 +31,7 @@ export function createBitmagnetDeployment(chart: Chart) {
     itemPath:
       "vaults/v64ocnykdqju4ui6j6pua56xw4/items/3fznikxjqt4szpz3ngdv462m6m",
     database: "bitmagnet",
-    size: Size.gibibytes(30),
+    size: Size.gibibytes(60),
   });
 
   const deployment = new Deployment(chart, "bitmagnet", {
