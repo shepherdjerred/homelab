6674c254e13f0b6893f27be750068b56e0e6b794
Jerred Shepherd <github@sjer.red>
2024-02-05 02:14:50 +0000

    limit palworld

--- cdk8s/src/services/games/palworld.ts
+++ cdk8s/src/services/games/palworld.ts
@@ -43,7 +43,7 @@ export function createPalworldDeployment(chart: Chart) {
       },
       resources: {
         memory: {
-          limit: Size.gibibytes(16),
+          limit: Size.gibibytes(12),
         },
       },
       envVariables: {
