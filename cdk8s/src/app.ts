de0c406e894178dc5c58878a7f09cccacb554215
Jerred <git@sjer.red>
2024-02-10 05:49:44 +0000

    add vol

--- cdk8s/src/app.ts
+++ cdk8s/src/app.ts
@@ -3,6 +3,7 @@ import { createLamportChart } from "./charts/lamport.ts";
 import { createGlitterBoysChart } from "./charts/glitter-boys.ts";
 import { createProjectChart } from "./charts/project.ts";
 import { createAppsChart } from "./charts/apps.ts";
+import { createImmichChart } from "./charts/immich.ts";
 
 const app = new App();
 
@@ -11,5 +12,6 @@ createGlitterBoysChart(app, "prod");
 createLamportChart(app);
 createProjectChart(app);
 createAppsChart(app);
+createImmichChart(app);
 
 app.synth();
