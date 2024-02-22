6ec0b6d19dd3770c1c127d412c0c383d9a6d84ff
Jerred <git@sjer.red>
2024-02-21 18:55:03 -0800

    use https

--- cdk8s/src/services/frontends/teddit.ts
+++ cdk8s/src/services/frontends/teddit.ts
@@ -29,7 +29,8 @@ export function createTedditDeployment(chart: Chart) {
       image: "teddit/teddit",
       envVariables: {
         REDIS_HOST: EnvValue.fromValue(redis.service.name),
-        DOMAIN: EnvValue.fromValue("https://teddit.tailnet-1a49.ts.net"),
+        DOMAIN: EnvValue.fromValue("teddit.tailnet-1a49.ts.net"),
+        HTTPS_ENABLED: EnvValue.fromValue("true"),
       },
       securityContext: {
         readOnlyRootFilesystem: false,
