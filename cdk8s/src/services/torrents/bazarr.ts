f1eae51886fd4d79db681232fb268d06158f5466
Jerred <git@sjer.red>
2024-02-21 20:22:00 -0800

    disable bazarr

--- cdk8s/src/services/torrents/bazarr.ts
+++ cdk8s/src/services/torrents/bazarr.ts
@@ -12,7 +12,7 @@ import { TailscaleIngress } from "../../utils/tailscale.ts";
 
 export function createBazarrDeployment(chart: Chart) {
   const deployment = new Deployment(chart, "bazarr", {
-    replicas: 1,
+    replicas: 0,
     strategy: DeploymentStrategy.recreate(),
   });
 
