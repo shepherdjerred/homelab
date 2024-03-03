78e7928410496285e22a1b46f8013ccd07a813e4
Jerred Shepherd <github@sjer.red>
2024-03-02 21:26:26 -0800

    update

--- cdk8s/src/services/glitter/glitter-boys.ts
+++ cdk8s/src/services/glitter/glitter-boys.ts
@@ -106,7 +106,7 @@ export function createBackendDeployment(chart: Chart, stage: Stage) {
 
   deployment.addContainer(withCommonProps({
     image:
-      "ghcr.io/shepherdjerred/glitter-boys:d7caeecd95e9d1a47063e3d41206001dedbb5945",
+      "ghcr.io/shepherdjerred/glitter-boys:3ee895fe9d012762fde4450b8319991c1b2b1bbf",
     ports: [
       {
         name: "port-8000",
