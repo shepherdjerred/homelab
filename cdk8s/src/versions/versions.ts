cbffabfb64dbe07126937d614a6557cc62da167a
Jerred Shepherd <github@sjer.red>
2024-05-17 19:06:56 -0700

    renovate again

--- cdk8s/src/versions/versions.ts
+++ cdk8s/src/versions/versions.ts
@@ -1,28 +1,23 @@
-// https://docs.renovatebot.com/modules/manager/regex/
 const versions = {
+  // renovate: datasource=helm versioning=helm
   "https://1password.github.io/connect-helm-charts/": "1.15.0",
   "https://github.com/argoproj/argo-cd": "stable",
   "https://charts.jetstack.io": "v1.14.4",
   "https://github.com/kubernetes-csi/external-snapshotter": "release-7.0",
   "https://github.com/immich-app/immich-charts/": "immich-0.6.0",
-  // renovate: datasource=docker packageName=immich-server versioning=docker
-  "immich": "v1.105.1",
+  "immich": "v1.103.1",
   "https://intel.github.io/helm-charts/": "0.29.0",
   "https://charts.jenkins.io": "5.1.6",
   "https://charts.bitnami.com/bitnami": "14.1.7",
   "https://kubernetes-sigs.github.io/node-feature-discovery/charts": "0.15.4",
   "https://prometheus-community.github.io/helm-charts": "58.0.0",
   "https://github.com/rancher/system-upgrade-controller/": "v0.13.4",
-  "https://pkgs.tailscale.com/helmcharts": "1.66.3",
+  "https://pkgs.tailscale.com/helmcharts": "1.62.1",
   "https://backube.github.io/helm-charts/": "0.9.1",
-  // renovate: datasource=docker packageName=postgres versioning=docker
-  "postgres": "16.2",
-  // renovate: datasource=docker packageName=redis versioning=docker
-  "redis": "7.2",
-  // renovate: datasource=docker packageName=earthly/satellite versioning=docker
-  "earthly": "v0.8.8",
-  // renovate: datasource=docker packageName=quay.io/invidious/invidious versioning=docker
-  "invidious": "latest",
+  "postgres": "postgres:16.2",
+  "redis": "redis:7.2",
+  "earthly": "earthly/satellite:v0.8.8",
+  "invidious": "quay.io/invidious/invidious",
   "teddit": "teddit/teddit",
   "minecraft": "itzg/minecraft-server",
   "palworld": "thijsvanloef/palworld-server-docker",
