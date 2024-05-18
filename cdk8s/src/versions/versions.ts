d0a4811ef3920375157761f54cf092d7a89a843a
Jerred Shepherd <github@sjer.red>
2024-05-18 14:07:15 -0700

    docker

--- cdk8s/src/versions/versions.ts
+++ cdk8s/src/versions/versions.ts
@@ -34,48 +34,48 @@ const versions = {
   // renovate: datasource=docker versioning=semver-coerced
   "earthly/satellite":
     "v0.8.11@sha256:6fd2b6b388405474bd85d9a0203ae8e5ff4ae7859d099b8a84978007dc0d2ebf",
-  // renovate: datasource=docker versioning=loose
+  // renovate: datasource=docker versioning=docker
   "invidious/invidious": "latest",
-  // renovate: datasource=docker versioning=loose
+  // renovate: datasource=docker versioning=docker
   "teddit/teddit": "latest",
-  // renovate: datasource=docker versioning=loose
+  // renovate: datasource=docker versioning=docker
   "itzg/minecraft-server": "latest",
-  // renovate: datasource=docker registryUrl=https://ghcr.io/ versioning=loose
+  // renovate: datasource=docker registryUrl=https://ghcr.io/ versioning=docker
   "thijsvanloef/palworld-server-docker": "latest",
   // renovate: datasource=docker registryUrl=https://ghcr.io/ versioning=semver
   "shepherdjerred/glitter-boys": "1.1.30",
   // renovate: datasource=docker registryUrl=https://ghcr.io/ versioning=semver
   "lavalink-devs/lavalink": "4",
-  // renovate: datasource=docker versioning=loose
+  // renovate: datasource=docker versioning=docker
   "plexinc/pms-docker": "latest",
-  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=loose
+  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=docker
   "linuxserver/tautulli": "latest",
-  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=loose
+  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=docker
   "linuxserver/bazarr": "latest",
-  // renovate: datasource=docker registryUrl=https://ghcr.io/ versioning=loose
+  // renovate: datasource=docker registryUrl=https://ghcr.io/ versioning=docker
   "bitmagnet-io/bitmagnet":
     "latest@sha256:c638773151d97e6bd2e19f181f0845be4c82893974975037aa06665c711e4466",
-  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=loose
+  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=docker
   "linuxserver/overseerr": "latest",
-  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=loose
+  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=docker
   "linuxserver/prowlarr": "latest",
   // renovate: datasource=docker registryUrl=https://ghcr.io/ versioning=semver-coerced
   "qdm12/gluetun": "v3.38",
-  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=loose
+  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=docker
   "linuxserver/qbittorrent": "latest",
-  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=loose
+  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=docker
   "linuxserver/radarr": "latest",
-  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=loose
+  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=docker
   "linuxserver/sonarr": "latest",
-  // renovate: datasource=docker versioning=loose
+  // renovate: datasource=docker versioning=docker
   "timothyjmiller/cloudflare-ddns": "latest",
-  //- renovate: datasource=docker versioning=loose
+  // renovate: datasource=docker versioning=docker
   "esphome/esphome": "2024.5.0",
-  // renovate: datasource=docker registryUrl=https://ghcr.io/ versioning=loose
+  // renovate: datasource=docker registryUrl=https://ghcr.io/ versioning=docker
   "tailscale/golink": "main",
-  // renovate: datasource=docker registryUrl=https://ghcr.io/ versioning=loose
+  // renovate: datasource=docker registryUrl=https://ghcr.io/ versioning=docker
   "home-assistant/home-assistant": "stable",
-  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=loose
+  // renovate: datasource=docker registryUrl=https://lscr.io/ versioning=docker
   "linuxserver/syncthing": "latest",
   // renovate: datasource=github-releases versioning=semver-coerced
   "windmill-labs/windmill-helm-charts": "windmill-2.0.181",
