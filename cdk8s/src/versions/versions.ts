0146d811c3fcd467c3c0c57949467538001a5faf
Jerred Shepherd <github@sjer.red>
2024-05-19 08:21:42 -0700

    fix sha

--- cdk8s/src/versions/versions.ts
+++ cdk8s/src/versions/versions.ts
@@ -104,7 +104,7 @@ const versions = {
   "windmill-labs/windmill-helm-charts": "windmill-2.0.181",
   // renovate: datasource=docker registryUrl=https://ghcr.io versioning=semver
   "lloesche/valheim-server":
-    "latest@sha256:85ab5dbb0e538cd7c711231384a1b78357ac6f85611219022e1be22e856d922f ",
+    "latest@sha256:85ab5dbb0e538cd7c711231384a1b78357ac6f85611219022e1be22e856d922f",
 };
 
 export default versions;
