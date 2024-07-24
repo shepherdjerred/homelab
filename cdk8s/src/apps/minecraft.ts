202a5d792b865be5057dc963a80bf2b5562bc40a
Jerred Shepherd <github@sjer.red>
2024-07-23 20:11:16 -0700

    fix: disable requests

--- cdk8s/src/apps/minecraft.ts
+++ cdk8s/src/apps/minecraft.ts
@@ -56,11 +56,7 @@ export function createMinecraftApp(chart: Chart) {
               value: "4G",
             },
             {
-              name: "resources.requests.memory",
-              value: "",
-            },
-            {
-              name: "resources.requests.cpu",
+              name: "resources.requests",
               value: "",
             },
             {
