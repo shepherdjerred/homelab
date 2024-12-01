68affd15eefc1f3dbb2827738496f5b41bd903f7
Jerred Shepherd <github@sjer.red>
2024-12-01 11:11:07 -0800

    fix: update Jenkins

--- cdk8s/src/versions/versions.ts
+++ cdk8s/src/versions/versions.ts
@@ -10,7 +10,7 @@ const versions = {
   // renovate: datasource=helm registryUrl=https://intel.github.io/helm-charts/ versioning=semver
   "intel-device-plugins-operator": "0.31.1",
   // renovate: datasource=helm registryUrl=https://charts.jenkins.io versioning=semver
-  "jenkins": "5.7.8",
+  "jenkins": "5.7.17",
   // renovate: datasource=helm registryUrl=https://charts.bitnami.com/bitnami versioning=semver
   "minio": "14.8.1",
   // renovate: datasource=helm registryUrl=https://kubernetes-sigs.github.io/node-feature-discovery/charts versioning=semver
