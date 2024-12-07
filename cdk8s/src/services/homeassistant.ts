c9343d32a1b974f54feff4fb1dbb5c87f80c5d84
Jerred Shepherd <github@sjer.red>
2024-12-07 11:29:00 -0800

    feat: try file

--- cdk8s/src/services/homeassistant.ts
+++ cdk8s/src/services/homeassistant.ts
@@ -68,20 +68,11 @@ export function createHomeAssistantDeployment(chart: Chart) {
     },
   });
 
-  const automationsYml = Deno.readTextFileSync(
-    "config/homeassistant/automations.yaml",
-  );
-  const configurationYml = Deno.readTextFileSync(
-    "config/homeassistant/configuration.yaml",
-  );
-  const scenesYml = Deno.readTextFileSync("config/homeassistant/scenes.yaml");
-  const scriptsYml = Deno.readTextFileSync("config/homeassistant/scripts.yaml");
-
   const config = new ConfigMap(chart, "ha-conf");
-  config.addData("automations.yaml", automationsYml);
-  config.addData("configuration.yaml", configurationYml);
-  config.addData("scenes.yaml", scenesYml);
-  config.addData("scripts.yaml", scriptsYml);
+  config.addFile("config/homeassistant/automations.yaml");
+  config.addFile("config/homeassistant/configuration.yaml");
+  config.addFile("config/homeassistant/scenes.yaml");
+  config.addFile("config/homeassistant/scripts.yaml");
 
   deployment.addContainer(
     withCommonProps({
