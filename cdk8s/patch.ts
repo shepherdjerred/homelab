2f90cd96b742bef678e229ce04ad886c40d07944
Jerred Shepherd <github@sjer.red>
2024-12-14 13:50:16 -0800

    fix: build

--- cdk8s/patch.ts
+++ cdk8s/patch.ts
@@ -1,11 +1,7 @@
 #!/usr/bin/env -S deno run --allow-run
 
 // on macOS use gsed, on Linux use sed
-const gsed = new Deno.Command("gsed", {
-  args: ["--version"],
-});
-const output = await gsed.output();
-const sedCommand = output.code === 0 ? "gsed" : "sed";
+const sedCommand = Deno.build.os === "darwin" ? "gsed" : "sed";
 
 // replace "gpu.intel.com/i915: null" with "gpu.intel.com/i915: 1" in dist/lamport.k8s.yaml
 const command = new Deno.Command(sedCommand, {
