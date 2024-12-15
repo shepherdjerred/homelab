d0f03437cd5f40e9cd4b1b36544a98a78ed64662
Jerred Shepherd <github@sjer.red>
2024-12-14 17:46:21 -0800

    feat: argocd-cm

--- cdk8s/src/apps/argocd.ts
+++ cdk8s/src/apps/argocd.ts
@@ -1,8 +1,23 @@
 import { Chart } from "cdk8s";
 import { Application } from "../../imports/argoproj.io.ts";
 import versions from "../versions/versions.ts";
+import { ConfigMap } from "cdk8s-plus";
 
 export function createArgoCdApp(chart: Chart) {
+  new ConfigMap(chart, "argocd-cm", {
+    metadata: {
+      name: "argocd-cm",
+      namespace: "argocd",
+    },
+    data: {
+      url: "https://argocd.tailnet-1a49.ts.net/",
+      "accounts.jenkins": "apiKey",
+      "accounts.jenkins.enabled": "true",
+      "exec.enabled": "true",
+      "timeout.reconciliation": "60s",
+    },
+  });
+
   new Application(chart, "argocd-app", {
     metadata: {
       name: "argocd",
