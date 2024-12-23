c0e52bae1d1fb1380eb0c16fa6d644fe07de6db9
Jerred Shepherd <github@sjer.red>
2024-12-23 11:20:14 -0800

    fix: config

--- cdk8s/src/apps/prometheus.ts
+++ cdk8s/src/apps/prometheus.ts
@@ -50,6 +50,29 @@ export function createPrometheusApp(chart: Chart) {
             },
             alertmanager: {
               config: {
+                global: {
+                  resolve_timeout: "5m",
+                },
+                inhibit_rules: [
+                  {
+                    source_matchers: ["severity = critical"],
+                    target_matchers: ["severity =~ warning|info"],
+                    equal: ["namespace", "alertname"],
+                  },
+                  {
+                    source_matchers: ["severity = warning"],
+                    target_matchers: ["severity = info"],
+                    equal: ["namespace", "alertname"],
+                  },
+                  {
+                    source_matchers: ["alertname = InfoInhibitor"],
+                    target_matchers: ["severity = info"],
+                    equal: ["namespace"],
+                  },
+                  {
+                    target_matchers: ["alertname = InfoInhibitor"],
+                  },
+                ],
                 receivers: [
                   {
                     name: "discord",
