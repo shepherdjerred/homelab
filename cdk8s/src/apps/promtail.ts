acf7cd4bb212c5d25991a681c00f5ec5364215da
Jerred Shepherd <github@sjer.red>
2025-01-12 10:21:24 -0800

    fix: update url

--- cdk8s/src/apps/promtail.ts
+++ cdk8s/src/apps/promtail.ts
@@ -19,8 +19,7 @@ export function createPromtailApp(chart: Chart) {
             config: {
               clients: [
                 {
-                  url:
-                    "http://loki.loki.svc.cluster.local:3100/loki/api/v1/push",
+                  url: "http://loki-gateway.loki:3100/loki/api/v1/push",
                 },
               ],
             },
