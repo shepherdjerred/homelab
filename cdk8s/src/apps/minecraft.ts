01101a765b3077dc86ab57959d9d1ff2f9214b2b
virmel <45904159+virmel@users.noreply.github.com>
2025-02-08 13:57:50 -0800

    Update minecraft.ts
    add Virmel to whitelist

--- cdk8s/src/apps/minecraft.ts
+++ cdk8s/src/apps/minecraft.ts
@@ -93,7 +93,7 @@ export function createMinecraftApp(chart: Chart) {
             },
             {
               name: "minecraftServer.whitelist",
-              value: "RiotShielder,lolopToaster",
+              value: "RiotShielder,lolopToaster, Virmel",
             },
             {
               name: "minecraftServer.spawnProtection",
