963466600467cb140da278d7a66603aa6491e641
Jerred Shepherd <github@sjer.red>
2025-03-13 22:44:13 -0700

    fix: update deps

--- cdk8s/vendor/esm.sh/#ts-pattern@5.6.0_357d3.js
+++
@@ -1,2 +0,0 @@
-/* esm.sh - ts-pattern@5.6.0 */
-export * from "/ts-pattern@5.6.0/denonext/ts-pattern.mjs";
--- cdk8s/vendor/esm.sh/ts-pattern@5.6.0/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import * as Pattern from './patterns.d.ts';
-export { match } from './match.d.ts';
-export { isMatching } from './is-matching.d.ts';
-export { Pattern, Pattern as P };
-export { NonExhaustiveError } from './errors.d.ts';
