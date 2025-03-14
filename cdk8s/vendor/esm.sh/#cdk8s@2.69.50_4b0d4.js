93e5a6dbe917de4188bbf2ccd3f22b71a465a9ab
Jerred Shepherd <github@sjer.red>
2025-03-13 22:54:31 -0700

    fix: deps

---
+++ cdk8s/vendor/esm.sh/#cdk8s@2.69.50_4b0d4.js
@@ -0,0 +1,4 @@
+/* esm.sh - cdk8s@2.69.50 */
+import "/constructs@^10?target=denonext";
+export * from "/cdk8s@2.69.50/denonext/cdk8s.bundle.mjs";
+export { default } from "/cdk8s@2.69.50/denonext/cdk8s.bundle.mjs";
