fc6a83e3c6aae9de0c30720a566aff9a34e74d32
Jerred Shepherd <github@sjer.red>
2025-03-23 14:22:10 -0700

    fix: port

--- cdk8s/src/services/pokemon.ts
+++ cdk8s/src/services/pokemon.ts
@@ -137,7 +137,7 @@ export function createPokemonDeployment(chart: Chart) {
 
   const uiService = new Service(chart, "ui-service", {
     selector: deployment,
-    ports: [{ port: 8083 }],
+    ports: [{ port: 9090 }],
   });
 
   new TailscaleIngress(chart, "ui-tailscale-ingress", {
