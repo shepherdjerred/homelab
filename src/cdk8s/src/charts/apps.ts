a5dafdad3757890b772c113d7eba327a61790c40
Jerred Shepherd <github@sjer.red>
2025-08-16 21:12:18 -0700

    ns

--- src/cdk8s/src/charts/apps.ts
+++ src/cdk8s/src/charts/apps.ts
@@ -43,6 +43,8 @@ export function createAppsChart(app: App) {
       name: `devpod`,
       labels: {
         "pod-security.kubernetes.io/audit": "privileged",
+        "pod-security.kubernetes.io/enforce": "privileged",
+        "pod-security.kubernetes.io/warn": "privileged",
       },
     },
   });
