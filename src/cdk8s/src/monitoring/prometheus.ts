3d1cb37bfcabad8260dea87840f85e3a0efda10d
Jerred Shepherd <github@sjer.red>
2025-09-18 02:38:52 +0000

    prom

--- src/cdk8s/src/monitoring/prometheus.ts
+++ src/cdk8s/src/monitoring/prometheus.ts
@@ -7,6 +7,7 @@ export function createPrometheusMonitoring(chart: Chart) {
     metadata: {
       name: "prometheus-homeassistant-rules",
       namespace: "prometheus",
+      labels: { release: "prometheus" },
     },
     spec: {
       groups: [
