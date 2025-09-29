023d52d6e84474a607296499a3f633c6adb68d22
Jerred Shepherd <github@sjer.red>
2025-09-29 12:14:58 -0700

    work on alertmanager

--- src/cdk8s/src/apps/prometheus.ts
+++ src/cdk8s/src/apps/prometheus.ts
@@ -203,26 +203,26 @@ export async function createPrometheusApp(chart: Chart) {
                   "{{ range .Alerts }}{{ .Annotations.summary }}\n{{ end }}",
                 ),
                 severity: "error",
-                details: escapeAlertmanagerTemplate(
-                  JSON.stringify(
-                    {
-                      firing: "{{ range .Alerts.Firing }}{{ . }}\n{{ end }}",
-                      resolved:
-                        "{{ range .Alerts.Resolved }}{{ . }}\n{{ end }}",
-                      num_firing: "{{ .Alerts.Firing | len }}",
-                      num_resolved: "{{ .Alerts.Resolved | len }}",
-                    },
-                    null,
-                    2,
-                  ),
-                ),
-                // Grafana has an image rendering feature
-                // let's see if we can use it here
-                images: [],
-                links: [],
-                component: "",
-                group: "",
-                class: "",
+                // details: escapeAlertmanagerTemplate(
+                //   JSON.stringify(
+                //     {
+                //       firing: "{{ range .Alerts.Firing }}{{ . }}\n{{ end }}",
+                //       resolved:
+                //         "{{ range .Alerts.Resolved }}{{ . }}\n{{ end }}",
+                //       num_firing: "{{ .Alerts.Firing | len }}",
+                //       num_resolved: "{{ .Alerts.Resolved | len }}",
+                //     },
+                //     null,
+                //     2,
+                //   ),
+                // ),
+                // // Grafana has an image rendering feature
+                // // let's see if we can use it here
+                // images: [],
+                // links: [],
+                // component: "",
+                // group: "",
+                // class: "",
               },
             ],
           },
