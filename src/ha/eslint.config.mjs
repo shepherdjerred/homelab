cf74cac21d720bd424c1c77fa0bb20015fb85022
Jerred Shepherd <github@sjer.red>
2025-10-02 02:38:14 +0000

    fix up ts config

--- src/ha/eslint.config.mjs
+++ src/ha/eslint.config.mjs
@@ -1,7 +1,8 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
