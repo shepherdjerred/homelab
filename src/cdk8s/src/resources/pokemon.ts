66907c1436f8c25a4d50f223edc7cf4e8b3b21d4
Jerred Shepherd <github@sjer.red>
2025-10-17 04:08:35 +0000

    fix: scale down pokemon

--- src/cdk8s/src/resources/pokemon.ts
+++ src/cdk8s/src/resources/pokemon.ts
@@ -18,7 +18,7 @@ export function createPokemonDeployment(chart: Chart) {
   const GID = 1000;
 
   const deployment = new Deployment(chart, "pokemon", {
-    replicas: 1,
+    replicas: 0,
     strategy: DeploymentStrategy.recreate(),
     securityContext: {
       fsGroup: GID,
