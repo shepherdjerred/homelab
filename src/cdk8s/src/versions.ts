de7e77a8062ff05fcf108ee6319cabeec7cba13d
scout-for-lol <github@sjer.red>
2025-11-04 03:59:51 +0000

    chore: update scout-for-lol version to 1.0.238

--- src/cdk8s/src/versions.ts
+++ src/cdk8s/src/versions.ts
@@ -66,7 +66,7 @@ const versions = {
   // renovate: datasource=helm registryUrl=https://openebs.github.io/openebs versioning=semver
   openebs: "4.3.3",
   // not managed by renovate
-  "shepherdjerred/scout-for-lol/beta": "1.0.237",
+  "shepherdjerred/scout-for-lol/beta": "1.0.238",
   // not managed by renovate
   "shepherdjerred/scout-for-lol/prod": "1.0.236",
   // not managed by renovate
