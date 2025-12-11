6db4cc47dca06f520b00ebf6e93f1476d77b6b49
Jerred Shepherd <github@sjer.red>
2025-12-10 21:13:07 -0800

    fix: update

--- src/cdk8s/src/versions.ts
+++ src/cdk8s/src/versions.ts
@@ -10,7 +10,7 @@ const versions = {
   // renovate: datasource=helm registryUrl=https://kubernetes-sigs.github.io/node-feature-discovery/charts versioning=semver
   "node-feature-discovery": "0.18.3",
   // renovate: datasource=helm registryUrl=https://prometheus-community.github.io/helm-charts versioning=semver
-  "kube-prometheus-stack": "79.12.0",
+  "kube-prometheus-stack": "80.2.0",
   // renovate: datasource=helm registryUrl=https://prometheus-community.github.io/helm-charts versioning=semver
   "prometheus-adapter": "5.2.0",
   // renovate: datasource=helm registryUrl=https://pkgs.tailscale.com/helmcharts versioning=semver
