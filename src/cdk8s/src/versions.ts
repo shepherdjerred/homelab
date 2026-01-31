a08e4639c6990dc9853a5492adf1f6ddbc76ae22
Jerred Shepherd <ShepherdJerred@gmail.com>
2026-01-31 11:29:35 -0800

    Update version for shepherdjerred/scout-for-lol/prod

--- src/cdk8s/src/versions.ts
+++ src/cdk8s/src/versions.ts
@@ -83,7 +83,7 @@ const versions = {
   openebs: "4.4.0",
   // not managed by renovate
   "shepherdjerred/scout-for-lol/beta": "1.0.222",
-  "shepherdjerred/scout-for-lol/prod": "1.0.207",
+  "shepherdjerred/scout-for-lol/prod": "1.0.222",
   // not managed by renovate
   "shepherdjerred/starlight-karma-bot/beta": "1.0.28",
   "shepherdjerred/starlight-karma-bot/prod": "1.0.13",
